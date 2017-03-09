9df59e267636d0fa964b6082017c767afca2557b
gombru <raulgombru@gmail.com>
2017-03-09 17:46:38 +0100

    first commit

--- process_tweets.py
+++ process_tweets.py
@@ -8,7 +8,6 @@ images_dir = '../../datasets/SocialMedia/img/'
 ann_dir = '../../datasets/SocialMedia/ann/'
 
 
-
 def download_save_image(url, filename):
     resource = urllib.urlopen(url)
     output = open(images_dir + filename, "wb")
@@ -28,8 +27,6 @@ for line in tweets_file:
 
 print "Number of tweets to process: " + str(len(tweets))
 
-# -- FILTER BY IMAGE AND SAVE IMAGES -- discard tweets without image
-print "Filtering by image ..."
 c = 0
 for t in tweets:
 
@@ -37,44 +34,31 @@ for t in tweets:
     if c % 100 == 0:
         print c
 
-    if t.has_key(u'entities'):
+    # -- FILTER BY IMAGE AND SAVE IMAGES -- discard tweets without image
+    if t.has_key(u'id') and t.has_key(u'entities'):
         if t['entities'].has_key(u'media'):
             if t['entities']['media'][0]['type'] == u'photo':
+                if t['entities']['media'][0]['media_url'][-3:] != 'jpg':
+                    continue
                 download_save_image(t['entities']['media'][0]['media_url'], str(t['id']) + ".jpg")
                 print t['entities']['media'][0]['media_url']
-                continue
 
-    tweets.remove(t)
+                # -- FILTER BY TEXT AND SAVE TEXT CONTENT -- discard short tweets
+                if t.has_key(u'id') and t.has_key(u'text') and t.has_key(u'created_at'):
 
-# -- FILTER BY TEXT AND SAVE TEXT CONTENT -- discard short tweets
-print "Filtering by text ..."
-c = 0
-for t in tweets:
+                    if len(t['text']) < min_text_length:
+                        print "Text too short: " + t['text']
+                        continue
 
-    c += 1
-    if c % 100 == 0:
-        print c
+                    hashtags_str = ''
+                    if t.has_key(u'entities'):
+                        for hashtag in t['entities']['hashtags']:
+                            hashtags_str = hashtags_str + ',' + hashtag['text']
 
-    if t.has_key(u'text'):
-        if len(t['text']) < min_text_length:
-            print "Text too short: " + t['text']
-            tweets.remove(t)
-    else:
-        tweets.remove(t)
-
-    hashtags_str = ''
-    if t.has_key(u'entities'):
-        for hashtag in t['entities']['hashtags']:
-            hashtags_str = hashtags_str + ',' + hashtag['text']
-
-    print t['id']
-
-    with open(images_dir + str(t['id']) + '.txt', "w") as text_file:
-        text_file.write(str(t['id']) + '\n' + t['created_at'].encode("utf8", "ignore") + '\n' + t['text'].encode("utf8", "ignore") + '\n' + hashtags_str[1:].encode("utf8", "ignore"))
-
-
-print "Number of remaining tweets after filtering: " + str(len(tweets))
-
+                    with open(ann_dir + str(t['id']) + '.txt', "w") as text_file:
+                        text_file.write(
+                            str(t['id']) + '\n' + t['created_at'].encode("utf8", "ignore") + '\n' + t['text'].encode(
+                                "utf8", "ignore").replace('\n', ' ').replace('\r', '') + '\n' + hashtags_str[1:].encode("utf8", "ignore"))
 
 
 
