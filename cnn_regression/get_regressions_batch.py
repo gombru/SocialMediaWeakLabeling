64e3e67bbb90fc29e71d992b78914786796e67f6
raulgomez <raulgombru@gmail.com>
2017-10-09 12:27:15 +0200

    scatter plot

--- cnn_regression/get_regressions_batch.py
+++ cnn_regression/get_regressions_batch.py
@@ -5,24 +5,24 @@ from PIL import Image
 import os
 
 # Run in GPU
-caffe.set_device(3)
+caffe.set_device(0)
 caffe.set_mode_gpu()
 
-# test = np.loadtxt('../../../datasets/SocialMedia/word2vec_tfidf_weighted_gt/test_InstaCities1M_divbymax.txt', dtype=str)
-test = np.loadtxt('../../../datasets/WebVision/info/test_filelist.txt', dtype=str)
+test = np.loadtxt('../../../datasets/SocialMedia/word2vec_mean_gt/test_InstaCities1M.txt', dtype=str)
+# test = np.loadtxt('../../../datasets/WebVision/info/test_filelist.txt', dtype=str)
 
 #Model name
-model = 'SocialMedia_Inception_frozen_glove_mean_iter_300000'
+model = 'WebVision_Inception_frozen_word2vec_tfidfweighted_divbymax_iter_460000'
 
 #Output file
-output_file_dir = '../../../datasets/WebVision/regression_output/' + model
+output_file_dir = '../../../datasets/SocialMedia/regression_output/' + model
 if not os.path.exists(output_file_dir):
     os.makedirs(output_file_dir)
 output_file_path = output_file_dir + '/test.txt'
 output_file = open(output_file_path, "w")
 
 # load net
-net = caffe.Net('../googlenet_regression/prototxt/deploy.prototxt', '../../../datasets/SocialMedia/models/saved/'+ model + '.caffemodel', caffe.TEST)
+net = caffe.Net('../googlenet_regression/prototxt/deploy.prototxt', '../../../datasets/WebVision/models/saved/'+ model + '.caffemodel', caffe.TEST)
 
 
 size = 227
@@ -46,8 +46,8 @@ while i < len(test):
         if i > len(test) - 1: break
 
         # load image
-        filename = '../../../datasets/WebVision/test_images_256/' + test[i]
-        # filename = '../../../datasets/SocialMedia/img_resized_1M/cities_instagram/' + test[i].split(',')[0] + '.jpg'
+        # filename = '../../../datasets/WebVision/test_images_256/' + test[i]
+        filename = '../../../datasets/SocialMedia/img_resized_1M/cities_instagram/' + test[i].split(',')[0] + '.jpg'
         im = Image.open(filename)
         im_o = im
         im = im.resize((size, size), Image.ANTIALIAS)
