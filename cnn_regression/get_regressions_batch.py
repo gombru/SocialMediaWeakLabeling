eaad43b767e9aa30832ad999cf891b5f75bd3bae
raulgomez <raulgombru@gmail.com>
2017-11-16 09:50:22 +0100

    Many things

--- cnn_regression/get_regressions_batch.py
+++ cnn_regression/get_regressions_batch.py
@@ -8,21 +8,22 @@ import os
 caffe.set_device(0)
 caffe.set_mode_gpu()
 
-test = np.loadtxt('../../../datasets/SocialMedia/word2vec_mean_gt/test_InstaCities1M.txt', dtype=str)
+test = np.loadtxt('../../../hd/datasets/instaBarcelona/word2vec_tfidf_weighted_gt/test_instaBarcelona_divbymax.txt', dtype=str)
+# test = np.loadtxt('../../../datasets/SocialMedia/word2vec_mean_gt/test_InstaCities1M.txt', dtype=str)
 # test = np.loadtxt('../../../datasets/WebVision/info/test_filelist.txt', dtype=str)
 
 #Model name
-model = 'WebVision_Inception_frozen_word2vec_tfidfweighted_divbymax_iter_460000'
+model = 'instaBCN_Inception_frozen_word2vec_tfidf_iter_60000'
 
 #Output file
-output_file_dir = '../../../datasets/SocialMedia/regression_output/' + model
+output_file_dir = '../../../hd/datasets/instaBarcelona/regression_output/' + model
 if not os.path.exists(output_file_dir):
     os.makedirs(output_file_dir)
 output_file_path = output_file_dir + '/test.txt'
 output_file = open(output_file_path, "w")
 
 # load net
-net = caffe.Net('../googlenet_regression/prototxt/deploy.prototxt', '../../../datasets/WebVision/models/saved/'+ model + '.caffemodel', caffe.TEST)
+net = caffe.Net('../googlenet_regression/prototxt/deploy.prototxt', '../../../datasets/instaBarcelona/models/CNNRegression/'+ model + '.caffemodel', caffe.TEST)
 
 
 size = 227
@@ -47,7 +48,7 @@ while i < len(test):
 
         # load image
         # filename = '../../../datasets/WebVision/test_images_256/' + test[i]
-        filename = '../../../datasets/SocialMedia/img_resized_1M/cities_instagram/' + test[i].split(',')[0] + '.jpg'
+        filename = '../../../hd/datasets/instaBarcelona/img_resized/' + test[i].split(',')[0] + '.jpg'
         im = Image.open(filename)
         im_o = im
         im = im.resize((size, size), Image.ANTIALIAS)
