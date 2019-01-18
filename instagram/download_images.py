e3f82f5d92f7122a4f129db93fee976d50178214
raulgomez <raulgombru@gmail.com>
2019-01-18 13:58:24 +0100

    Many things

--- instagram/download_images.py
+++ instagram/download_images.py
@@ -1,44 +1,24 @@
-# Downloads instagram images and captions given query words.
-# It's web-based, it does a search and goes page by page retrivieng results
-# Once in page 3XXX the results start to be repeated. So this script checks if the file to be downloaded already exists,
-# It stops downloading when N already existihuesng images have been tried to download.
-# It fires 4 ever every X minutes, so new uploaded images have been uploaded and can be dowloaded as the first results.
-# Tried to do a controled navigation of page results, but did not work, still repeated results.
-
 import os
-import hues
 import time
-from core import InstaLooter
 import requests.packages.urllib3
-import datetime
+import random
+
+print("RUN WITH PYTHON 3!!")
+
 requests.packages.urllib3.disable_warnings()
-directory = '../../../datasets/img/'
+directory = '../../../hd/datasets/insta10YearsChallenge/data/'
+# queries_file = '../instagram/queries_xmas.txt'
+queries = []
+# for line in open(queries_file,'r'):
+#     queries.append(line.strip('\n'))
+queries.append("10yearschallenge")
 get_videos = False
-first = True
-login1 = login = 'lazarpitas'
-password1 = password ='Gata20'
-login2 = 'lazarpitas'
-password2 = 'Gata20'
-new_only = False # Download only images newer than the current images in folder
-# timeframe: Download images between these dates
-# start_date = datetime.date.today()
-# start_day = 1
-# end_day = 21
-# end_date = start_date.replace(day=start_day)
-# start_date = start_date.replace(day=end_day)
-# timeframe = (start_date,end_date)
-timeframe = None
 
-# cities = ['amusement','anger','awe','contentment','disgusting','exciting','excitement','fear','sadness','fun','diversion','angry','wrath','rage','admiration','apprehension','astonishment','shock','complacency','pleasure','abominable','awful','distasteful','hateful','breathtaking','stimulating','thrilling','scare','scary','terror','anguish','grief','melancholy','dolor']
-cities = ['None']
-jobs = 32 #16
-num_2_query_first = 2500000 #Num of images to build the looter the first time (will downlaod old images)
+num_2_query_first = 1000000 #Num of images to build the looter the first time (will downlaod old images)
 num_2_query_next = 100000 #Num of images to build the looter affter (will download new updated images)
-num_2_download = 5000000 #Num of images we want to end up having
-
-sleep_seconds = 60*60
-
-
+num_2_download = 2000000 #Num of images we want to end up having
+sleep_seconds = 60 #120*60
+first = True
 num_2_query = num_2_query_first
 
 while True:
@@ -50,43 +30,25 @@ while True:
     first = False
     c = 0
 
-    for c in range(0, len(cities)):
+    # for c in range(0, len(queries)):
+    query_index = random.randint(0,len(queries)-1)
+    query = queries[query_index]
+    query_dir = directory #+ query + '/'
+    print("Downloading images for: " + query)
 
-        query = cities[c]
-        city_dir = directory #+ query + '/'
+    if not os.path.exists(query_dir):
+        os.makedirs(query_dir)
 
-        if not os.path.exists(city_dir):
-            os.makedirs(city_dir)
-        if not os.path.exists(city_dir.replace('img','json')):
-            os.makedirs(city_dir.replace('img','json'))
+    remaining_num_2_download = num_2_download - len(os.listdir(query_dir))
+    if remaining_num_2_download < 0: remaining_num_2_download = 0
 
-        remaining_num_2_download = num_2_download - len(os.listdir(city_dir))
-        if remaining_num_2_download < 0: remaining_num_2_download = 0
+    print "Number of images for " + query + ': ' + str(len(os.listdir(query_dir))) + "  --  Remaining: " + str(remaining_num_2_download)
 
-        print "Number of images for " + query + ': ' + str(len(os.listdir(city_dir))) + "  --  Remaining: " + str(remaining_num_2_download)
+    if remaining_num_2_download < 1:
+        continue
 
-        if remaining_num_2_download < 1:
-            continue
-        query = None
-        looter = InstaLooter(directory=city_dir, hashtag=query,
-            add_metadata=False, get_videos=get_videos, jobs=jobs)
+    try:
+        os.system("instalooter hashtag " + query + " " + directory + " -n " + str(num_2_query) + " -j 32  -d " )
 
-        # try:
-        looter.login(login, password)
-        hues.success('Logged in.')
-        # except:
-        #     print "Error while logging in"
-        #     if login == login1:
-        #         login = login2
-        #         password = password2
-        #     else:
-        #         login = login1
-        #         password = password1
-        #
-        #     time.sleep(60)
-        #     continue
-
-        # try:
-        looter.download(media_count=num_2_query, new_only = new_only, with_pbar=False)
-        # except:
-        #     print "Error while downloading, continuing ... "
+    except:
+        print("Error on lotter, continuing")
---
+++ instagram_old/download_images.py
@@ -0,0 +1,92 @@
+# Downloads instagram images and captions given query words.
+# It's web-based, it does a search and goes page by page retrivieng results
+# Once in page 3XXX the results start to be repeated. So this script checks if the file to be downloaded already exists,
+# It stops downloading when N already existihuesng images have been tried to download.
+# It fires 4 ever every X minutes, so new uploaded images have been uploaded and can be dowloaded as the first results.
+# Tried to do a controled navigation of page results, but did not work, still repeated results.
+
+import os
+import hues
+import time
+from core import InstaLooter
+import requests.packages.urllib3
+import datetime
+requests.packages.urllib3.disable_warnings()
+directory = '../../../datasets/img/'
+get_videos = False
+first = True
+login1 = login = 'lazarpitas'
+password1 = password ='Gata20'
+login2 = 'lazarpitas'
+password2 = 'Gata20'
+new_only = False # Download only images newer than the current images in folder
+# timeframe: Download images between these dates
+# start_date = datetime.date.today()
+# start_day = 1
+# end_day = 21
+# end_date = start_date.replace(day=start_day)
+# start_date = start_date.replace(day=end_day)
+# timeframe = (start_date,end_date)
+timeframe = None
+
+# cities = ['amusement','anger','awe','contentment','disgusting','exciting','excitement','fear','sadness','fun','diversion','angry','wrath','rage','admiration','apprehension','astonishment','shock','complacency','pleasure','abominable','awful','distasteful','hateful','breathtaking','stimulating','thrilling','scare','scary','terror','anguish','grief','melancholy','dolor']
+cities = ['None']
+jobs = 32 #16
+num_2_query_first = 2500000 #Num of images to build the looter the first time (will downlaod old images)
+num_2_query_next = 100000 #Num of images to build the looter affter (will download new updated images)
+num_2_download = 5000000 #Num of images we want to end up having
+
+sleep_seconds = 60*60
+
+
+num_2_query = num_2_query_first
+
+while True:
+
+    if not first:
+        num_2_query = num_2_query_next
+        print "Sleeping for: " + str(sleep_seconds / 60) + " minutes"
+        time.sleep(sleep_seconds)
+    first = False
+    c = 0
+
+    for c in range(0, len(cities)):
+
+        query = cities[c]
+        city_dir = directory #+ query + '/'
+
+        if not os.path.exists(city_dir):
+            os.makedirs(city_dir)
+        if not os.path.exists(city_dir.replace('img','json')):
+            os.makedirs(city_dir.replace('img','json'))
+
+        remaining_num_2_download = num_2_download - len(os.listdir(city_dir))
+        if remaining_num_2_download < 0: remaining_num_2_download = 0
+
+        print "Number of images for " + query + ': ' + str(len(os.listdir(city_dir))) + "  --  Remaining: " + str(remaining_num_2_download)
+
+        if remaining_num_2_download < 1:
+            continue
+        query = None
+        looter = InstaLooter(directory=city_dir, hashtag=query,
+            add_metadata=False, get_videos=get_videos, jobs=jobs)
+
+        # try:
+        looter.login(login, password)
+        hues.success('Logged in.')
+        # except:
+        #     print "Error while logging in"
+        #     if login == login1:
+        #         login = login2
+        #         password = password2
+        #     else:
+        #         login = login1
+        #         password = password1
+        #
+        #     time.sleep(60)
+        #     continue
+
+        # try:
+        looter.download(media_count=num_2_query, new_only = new_only, with_pbar=False)
+        # except:
+        #     print "Error while downloading, continuing ... "
